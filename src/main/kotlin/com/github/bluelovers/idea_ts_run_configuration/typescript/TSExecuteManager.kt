394eb67b16026e5621285abe22d03bd1bfd1a0e7
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-29 16:17:11 +0100

    add service that keeps track of project settings

---
+++ src/main/kotlin/com/github/bluelovers/idea_ts_run_configuration/typescript/TSExecuteManager.kt
@@ -0,0 +1,47 @@
+package com.github.bluelovers.idea_ts_run_configuration.typescript
+
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.javascript.nodejs.util.NodePackage
+import com.intellij.javascript.nodejs.util.NodePackageRef
+import com.intellij.openapi.project.Project
+import com.twelvemonkeys.lang.StringUtil
+
+class TSExecuteManager(private val project: Project) {
+	@Volatile
+	var tsExecutePackageRef: NodePackageRef = getDefaultPackageRef()
+		set(value) {
+			field = value
+			var identifier: String? = value.identifier
+			if (StringUtil.isEmpty(identifier)) {
+				identifier = null
+			}
+			PropertiesComponent.getInstance(project).setValue(TS_EXECUTE_PACKAGE_REF_PROPERTY_KEY, identifier)
+		}
+
+	private fun getDefaultPackageRef(): NodePackageRef {
+		val propertiesComponent = PropertiesComponent.getInstance(project)
+		val identifier = propertiesComponent.getValue(TS_EXECUTE_PACKAGE_REF_PROPERTY_KEY)
+
+		if (!StringUtil.isEmpty(identifier)) return TSExecuteUtil.DESCRIPTOR.createPackageRef(identifier!!)
+		if (project.isDefault) return NodePackageRef.create(NodePackage(""))
+
+		val packageRef = detectPackageRef()
+		propertiesComponent.setValue(TS_EXECUTE_PACKAGE_REF_PROPERTY_KEY, identifier)
+		return packageRef;
+	}
+
+	private fun detectPackageRef(): NodePackageRef {
+		val detectedPackageRef = detectInstalledTsExecutePackage()
+		if (detectedPackageRef != null) return detectedPackageRef
+
+		return NodePackageRef.create(TSExecuteUtil.TS_NODE_PACKAGE_NAME)
+	}
+
+	private fun detectInstalledTsExecutePackage(): NodePackageRef? =
+		TSExecuteUtil.DESCRIPTOR.listPackageRefs(project, null, null).firstOrNull()
+
+	companion object {
+		private val TS_EXECUTE_PACKAGE_REF_PROPERTY_KEY = "typescript_execute_package_path"
+		fun getInstance(project: Project): TSExecuteManager = project.getService(TSExecuteManager::class.java)
+	}
+}
